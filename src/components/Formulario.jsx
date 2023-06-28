8c3b2f086a188a4497f7649f3c844877951f858c
Sebastián Jerez Marín <104412736+SerapioUnlucky@users.noreply.github.com>
2023-06-28 19:43:50 -0400

    Letra mayúscula

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -112,7 +112,7 @@ export const Formulario = ({ setCotizacion, setCargando }) => {
 
     const monedas = [
         { id: 'USD', nombre: 'Dolar de Estados Unidos' },
-        { id: 'CLP', nombre: 'Peso chileno' },
+        { id: 'CLP', nombre: 'Peso Chileno' },
         { id: 'EUR', nombre: 'Euro' },
         { id: 'GBP', nombre: 'Libra Esterlina' }
     ];
