6454b9f865e0fcd41858b4af79fb2b3d1ddffdc9
SerapioUnlucky <sjerez957@gmail.com>
2024-03-08 00:54:42 -0300

    Se elimino el peso chileno

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -112,7 +112,6 @@ export const Formulario = ({ setCotizacion, setCargando }) => {
 
     const monedas = [
         { id: 'USD', nombre: 'Dolar de Estados Unidos' },
-        { id: 'CLP', nombre: 'Peso Chileno' },
         { id: 'EUR', nombre: 'Euro' },
         { id: 'GBP', nombre: 'Libra Esterlina' }
     ];
